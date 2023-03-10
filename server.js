776b9096e85461abebbf08e1bd0cf02ed58c4e3b
novahorizon <cpu.mage@gmail.com>
2023-03-10 04:02:33 -0700

    Recoded for websockets

--- server.js
+++ server.js
@@ -1,5 +1,3 @@
-// this is just to demo the concept of SSE, not intended for production usage.
-
 const http = require("http");
 var os = require("os");
 var fs = require("fs");
@@ -9,31 +7,14 @@ const port = 8080;
 const streamer = "" //left blank for Git
 
 let main;
-fs.readFile(__dirname + "/test.html", 'utf8', (err, data) => {main = data;});
-// A simple dataSource that changes over time
+fs.readFile(__dirname + "/overlay.html", 'utf8', (err, data) => {main = data;});
+
+
 let dataSource = -100;
-// const updateDataSource = () => {
-//   const delta = Math.random();
-//   dataSource += delta;
-// }
-
 let newtimer = false;
-const requestListener = function (req, res) {
-  if (req.url === '/chat') {
-    res.statusCode = 200;
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    res.setHeader("Cache-Control", "no-cache");
-    //res.setHeader("connection", "keep-alive");
-    res.setHeader("Content-Type", "text/event-stream");
 
-    setInterval(() => {
-        if (newtimer) {
-            newtimer = false;
-            const data = JSON.stringify({ timer: dataSource });
-            console.log(`SENDING: ${data}`);
-            res.write(`id: ${(new Date()).toLocaleTimeString()}\ndata: ${data}\n\n`);
-    }}, 1000);
-  } else {
+const requestListener = function (req, res) {
+    if (req.url === '/') {
     res.statusCode = 200;
     res.setHeader("Access-Control-Allow-Origin", "*");
     res.setHeader("Cache-Control", "no-cache");
@@ -41,16 +22,35 @@ const requestListener = function (req, res) {
     res.setHeader("Content-Type", "text/html");
     res.end(main);
     console.log(main);
-  }
+    } else {
+        console.log(`bad request: ${req.url}`);
+        res.statusCode = 404;
+        res.end();
+    }
 };
+
+
+const WebSocket = require('ws');
+const wss = new WebSocket.Server({port: 8079});
+
+wss.on('connection', (ws) => {
+    setInterval(() => {
+        if (newtimer) {
+            newtimer = false;
+            const data = JSON.stringify({ timer: dataSource });
+            ws.send(data);
+    }}, 100);
+});
+
 
 const server = http.createServer(requestListener);
 server.listen(port, host, () => {
-  //setInterval(()=> updateDataSource(), 500);
   console.log(`server running at http://${host}:${port}`);
 });
 
 const tmi = require('tmi.js');
+const { json } = require("stream/consumers");
+const { createHash } = require("crypto");
 
 const client = new tmi.Client({
 	channels: [streamer]
